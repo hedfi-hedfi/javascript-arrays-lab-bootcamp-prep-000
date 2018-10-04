7d9e84935acbad4e2bf61d4da3eee323616ccea1
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-04 10:06:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name){}
+
+function destructivelyPrependKitten(name){}
+
+function destructivelyPrependKitten(name){}
+
+function destructivelyPrependKitten(name){}
