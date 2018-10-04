81e59b287abec63b1d4f176f19b51dee2b1da85c
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-04 10:09:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 // Add your functions and code here
 kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(name){}
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
 
-function destructivelyPrependKitten(name){}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
 
-function destructivelyRemoveLastKitten(){}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
 
-function destructivelyRemoveFirstKitten(){}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
 
-function appendKitten(name) {}
+function appendKitten(name) {
+  
+}
 
 function prependKitten(name) {}
+
+function removeLastKitten() {}
+
+function removeFirststKitten() {}
